0c0d7635e5977dda30a5d78bc430ee6a39c286da
Kaveri Biswas <biswaskaveri66@gmail.com>
2021-10-24 18:07:39 +0530

    added sorting and row grouping functions

--- jsonapi.js
+++ jsonapi.js
@@ -1,53 +1,85 @@
-function tableData() {
-    try{
-        const app = document.getElementById('app');
+$(document).ready(function() {
+    let posts = [];
+    let table = document.getElementById('myTable');
 
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(posts => {
-            return posts.json();
-        })
-        .then(data => {
-            console.log("data: ", data);
-            // app.innerHTML = listOfPosts(data);
-            // Extract value from table header
-            let col = [];
-            for (let i = 0; i < data.length; i++){
-                for (let key in data[i]){
-                    if(col.indexOf(key) === -1){
-                        col.push(key);
-                    }
-                }
-            }
+    $.ajax({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        success: function(response) {
+            // console.log(response);
+            posts = response
+            // console.log(posts);
+            buildTable(posts)
+        }
+    });
 
-            // Creating table
-            const table = document.createElement("table");
+    function buildTable(data) {
+        // console.log(data);
+        table.innerHTML = "";
+        for (let i = 0; i < data.length; i++) {
+            let row = `<tr>
+                            <td>${data[i].userId}</td>
+                            <td>${data[i].id}</td>
+                            <td>${data[i].title}</td>
+                            <td>${data[i].body}</td>
+                        </tr>`;
 
-            // Create table header row using extracted headers above
-            let tr = table.insertRow(-1); 
-            let th;
-            for (let i = 0; i < col.length; i++){
-                th = document.createElement("th");
-                th.innerHTML = col[i];
-                tr.appendChild(th);
-            }
-
-            // adding json data to table rows
-            for (let i = 0; i < data.length; i++){
-                tr = table.insertRow(-1);
-                for (let j = 0; j < col.length; j++){
-                    let tableCell = tr.insertCell(-1);
-                    tableCell.innerHTML = data[i][col[j]];
-                }
-            }
-
-            //Adding to div
-            app.innerHTML = "";
-            app.appendChild(table);
-        })
-    } catch (e){
-        app.innerHTML = "title not found";
+            table.innerHTML += row;
+        }
     }
-}
 
-tableData();
+    // sorting by title
+    $('#title').on('click', function(){
+        let column = $(this).data('column');
+        let order = $(this).data('order');
+        let text = $(this).html();
+        text = text.substring(0, text.length - 1);
+        // console.log("clicked header!!", column, order);
 
+        if (order == 'desc') {
+            $(this).data('order', "asc");
+            posts = posts.sort((a,b) => a[column] > b[column] ? 1 : -1);
+            text += '&#9660';
+        } else {
+            $(this).data('order', "desc");
+            posts = posts.sort((a,b) => a[column] < b[column] ? 1 : -1);
+            text += '&#9650';
+        }
+        $(this).html(text);
+        buildTable(posts);
+    });
+
+    // buildTable(posts);
+
+    // grouping by userID
+    // NOTE: it works when the userID is sorted.
+    $('#userid').on('click', function(){
+        function groupTable($rows, startIndex, total) {
+            if (total === 0) return;
+            let i, currentIndex = startIndex, count = 1, myArray = [];
+            let tds = $rows.find('td:eq(' + currentIndex + ')');
+            let ctrl = $(tds[0]);
+            myArray.push($rows[0]);
+            for (i = 1; i <= tds.length; i++) {
+                if(ctrl.text() == $(tds[i]).text()) {
+                    count++;
+                    $(tds[i]).addClass('deleted');
+                    myArray.push($rows[i]);
+                } else {
+                    if (count > 1) {
+                        ctrl.attr('rowspan', count);
+                        groupTable($(myArray), startIndex + 1, total - 1);
+                    }
+                    count = 1;
+                    myArray = [];
+                    ctrl = $(tds[i]);
+                    myArray.push($rows[i]);
+                }
+            } 
+        }
+
+        groupTable($('#myTable tr:has(td)'),0,3);
+        $('#myTable .deleted').remove();
+    });
+});
+    
